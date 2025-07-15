559e750bb9f54985973a2b919833e74a8eb8e85e
tndals11 <136726862+tndals11@users.noreply.github.com>
2025-07-15 17:35:02 +0900

    20250715_PmJavaStudy
    싱글톤(singleton) 패턴에 사용목적, 장단점에 대해서 학습
    Gradle에 Lombok 설정하는법과 Lombok사용법에 대해서 학습

---
+++ _오신혁/Iot_Java/k1_iot_java/src/main/java/org/example/chapter13/F_Builder.java
@@ -0,0 +1,78 @@
+package org.example.chapter13;
+
+/*
+=== 빌더로 게임 캐릭터 생성===
+1) 필수: 이름(String), 분류(String)
+2) 선택: 스킬(String), 아이템(String)
+* */
+
+class GameCharacter {
+    private final String name;
+    private final String sort;
+
+    // 선택 사항
+    private final String skill;
+    private final String item;
+
+
+    // 1) 빌더 클래스
+    public static class Builder {
+        private final String name;
+        private final String sort;
+
+        private String skill = "Basic Attack";
+        private String item = "None";
+
+        public Builder(String name, String sort) {
+            this.name = name;
+            this.sort = sort;
+        }
+
+        // 선택필드에 대한 할당 메서드(필드명 그 자체의 메서드명을 가짐)
+        public Builder skill(String skill) {
+            this.skill = skill;
+            return this;
+        }
+
+        public Builder item(String item) {
+            this.item = item;
+            return this;
+        }
+
+        public GameCharacter build() {
+            return new GameCharacter(this);
+        }
+
+    }
+    public GameCharacter(Builder builder) {
+        this.name = builder.name;
+        this.sort = builder.sort;
+        this.skill = builder.skill;
+        this.item = builder.item;
+    }
+
+    @Override
+    public String toString() {
+        return "이름 : " + name + "분류 : " + sort + "스킬 : " + skill + " 아이템 : " + item;
+    }
+}
+
+
+public class F_Builder {
+    public static void main(String[] args) {
+        GameCharacter pororo = new GameCharacter.Builder("뽀로로", "펭귄")
+                .skill("크롱놀리기")
+                .item("안경")
+                .build();
+
+        GameCharacter loopy = new GameCharacter.Builder("루피", "비버")
+                .skill("정색")
+                .item("잔망루피")
+                .build();
+        System.out.println(pororo.toString());
+        System.out.println(loopy.toString());
+
+
+
+    }
+}
